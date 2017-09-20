89bf6a8d9a8ccbddee580822207cbfec9710f3b6
htmldrum <htmldrum@tutanota.com>
2017-09-20 13:21:53 +1000

    Reaaaaally didn't like the name of the post on scheme. I have no idea.

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,15 +4,14 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = ("/blog" + createFilePath({ node, getNode, basePath: `posts` })).slice(0, -1);
-    const dateMatch = node.id.match(/[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/)
+//    const slug = ("/blog" + createFilePath({ node, getNode, basePath: `posts` })).slice(0, -1);
+    const slug = ("/blog" + createFilePath({ node, getNode, basePath: `posts` }))
+    const dateMatch = node.id.match(/[0-9]{4}\-[0-9]{2}\-[0-9]{2}/)
     if(dateMatch == null) {
       throw Error(`Unable to find a jekyll-compliant date in ${node.id}`)
     }
     const date = new Date(dateMatch[0])
 
-    // TODO parse the following
-    // description
     createNodeField({
       node,
       name: `slug`,
@@ -54,16 +53,13 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
                   }
                 }
               }
-            }
-            `).then(result => {
+            }`
+            ).then(result => {
               result.data.allMarkdownRemark.edges.map(({ node }) => {
                 createPage({
-                  path: (node.fields.slug),
+                  path: node.fields.slug,
                   component: path.resolve(`./src/templates/post.js`),
-                  context: {
-                    // Data passed to context is available in page queries as GraphQL variables.
-                    slug: node.fields.slug
-                  }
+                  context: { slug:  node.fields.slug }
                 })
               })
               resolve()
--- src/templates/post.js
+++ src/templates/post.js
@@ -3,19 +3,26 @@ import React from "react"
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
-      <b>{ post.fields.slug }</b>
+      <div>
+        <h1>
+          {post.frontmatter.title}
+        </h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+     </div>
   )
 }
 
 export const query = graphql`
-query BlogPostQuery {
-  markdownRemark {
+query MyQueryName($slug: String!) {
+  markdownRemark(fields: { slug: { eq: $slug } }) {
     html
     frontmatter {
       title
     }
     fields {
-      slug
+     slug
     }
   }
-}`
+}
+`
+
